c167a1a8a573cbbd215b4a852bfddfa4ff651a1a
costilladante <dante.costilla@carrier.com>
2021-09-22 17:10:51 -0300

    Replace BrowserRouter by HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Home from "./views/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import PostSection from "./views/postSection/PostSection";
 import HomeHeader from "./views/home/homeHeader/HomeHeader";
